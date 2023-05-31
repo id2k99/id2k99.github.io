2d8baf002ba6e28b38957cd44bffd1da20f09b0c
sergey <sergey@solaev.ru>
2023-05-31 19:59:48 +0400

    create repo

---
+++ script/script.js
@@ -0,0 +1,20 @@
+function helloUser1() {
+    let userName = prompt("Введите Ваше имя:");
+    alert(`Привет ${userName}`);
+}
+
+function helloUser2() {
+    let userName = document.getElementById("userName").value;
+    document.getElementById("result").innerText = `Привет ${userName}`;
+}
+
+function send() {
+    let message = document.getElementById("chatText").value;
+    let li1 = document.createElement('li');
+    li1.innerHTML = message;
+    li1.className = "msg";
+    document.getElementById("chatResult").prepend(li1);
+    console.log(message);
+}
+
+helloUser1();
